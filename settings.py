0edef335192fc3a365ce5ef634b20dd6c3791c30
Mark Achiles <markachilesflores2004@gmail.com>
2023-08-14 21:20:56 +0800

    Cleaner version pero messy parin code

--- settings.py
+++ settings.py
@@ -1,11 +1,5 @@
 import os
-import pathlib
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-
-BASE_DIR = pathlib.Path(__file__).parent
-
-CMDS_DIR = BASE_DIR / "cmds"
-COGS_DIR = BASE_DIR / "cogs"
