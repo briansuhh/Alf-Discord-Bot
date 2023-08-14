8632f4be0ecf21cd2aafaa8e25c51bbff0691afd
Mark Achiles <markachilesflores2004@gmail.com>
2023-08-14 21:42:24 +0800

    Fixed IDs and placed remove reaction to the end of the program

--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+VERIFY_CHANNEL_TOKEN = int(os.environ["VERIFY_CHANNEL"])
+WELCOME_CHANNEL_TOKEN = int(os.environ["WELCOME_CHANNEL"])
